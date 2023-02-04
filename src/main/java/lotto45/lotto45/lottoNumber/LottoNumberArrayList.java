1f8da1d814b546e4f54dbadf0d8cebc9688cac8e
geomusteel <geomusteel@naver.com>
2023-02-05 01:16:33 +0900

    LottoNumberArrayList생성

---
+++ src/main/java/lotto45/lotto45/lottoNumber/LottoNumberArrayList.java
@@ -0,0 +1,13 @@
+package lotto45.lotto45.lottoNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoNumberArrayList {
+
+    List<Integer> LottoNum45 = new ArrayList<>();
+    for (int i = 0; i < 46 ; i++){
+        System.out.println(i);
+
+    }
+} // end class
