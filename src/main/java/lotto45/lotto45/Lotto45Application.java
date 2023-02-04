07015958036f57a26aa77ce8ed3b687d13584f42
geomusteel <geomusteel@naver.com>
2023-02-05 01:02:58 +0900

    start

---
+++ src/main/java/lotto45/lotto45/Lotto45Application.java
@@ -0,0 +1,13 @@
+package lotto45.lotto45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lotto45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lotto45Application.class, args);
+	}
+
+}
---
+++ src/test/java/lotto45/lotto45/Lotto45ApplicationTests.java
@@ -0,0 +1,13 @@
+package lotto45.lotto45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lotto45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
