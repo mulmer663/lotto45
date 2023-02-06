fe44f9d2b5651998db1034ca00780ca0e2818d7b
Mulmer <mulmer663@gmail.com>
2023-02-06 23:52:47 +0900

    🔨 뷰 부분 구현 수정

--- src/main/java/lotto45/lotto45/controller/LottoNumberController.java
+++ src/main/java/lotto45/lotto45/controller/LottoNumberController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
