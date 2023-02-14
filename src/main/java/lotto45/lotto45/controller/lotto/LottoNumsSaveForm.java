605244aec2c7e709700e6fd3cbffb817c94d5727
Mulmer <mulmer663@gmail.com>
2023-02-14 19:30:01 +0900

    🔨 당첨 통계 컨트롤러와 뷰 구현

---
+++ src/main/java/lotto45/lotto45/controller/lotto/LottoNumsSaveForm.java
@@ -0,0 +1,14 @@
+package lotto45.lotto45.controller.lotto;
+
+import lombok.Data;
+
+@Data
+public class LottoNumsSaveForm {
+
+    private Integer num1;
+    private Integer num2;
+    private Integer num3;
+    private Integer num4;
+    private Integer num5;
+    private Integer num6;
+}
--- src/main/java/lotto45/lotto45/controller/lotto/WinSttController.java
+++ src/main/java/lotto45/lotto45/controller/lotto/WinSttController.java
@@ -6,6 +6,7 @@ import lotto45.lotto45.service.lotto.WinSttInfoDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -24,24 +25,27 @@ public class WinSttController {
      * <p>따라서 로그인 후 해당 컨트롤러에 접근할 수 있고 1~3위 로직은 따로 서비스로 구현</p>
      */
     @GetMapping("/WinSttCheck")
-    public String goLottoNumber(Model model) {
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < 6; i++) {
-            list.add(0);
-        }
-        model.addAttribute("list", list);
-        return "lotto/createWinStt";
+    public String saveWinSttForm(@ModelAttribute("form") LottoNumsSaveForm form) {
+        return "lotto/saveWinSttForm";
     }
 
     @PostMapping("/WinSttCheck")
-    public String checkLottoNumber(List<Integer> lottoNums, Model model) {
+    public String checkLottoNumber(LottoNumsSaveForm form, Model model) {
+
+        List<Integer> lottoNums = new ArrayList<>();
+        lottoNums.add(form.getNum1());
+        lottoNums.add(form.getNum2());
+        lottoNums.add(form.getNum3());
+        lottoNums.add(form.getNum4());
+        lottoNums.add(form.getNum5());
+        lottoNums.add(form.getNum6());
+
         List<WinSttInfoDTO> winSttInfoDTOS = this.winSttService.calculateSttInfo(lottoNums);
 
         if (winSttInfoDTOS.size() == 0) {
-            model.addAttribute("NoWinStt", "당첨 통계가 없습니다.");
+            model.addAttribute("noWinStt", "당첨 통계가 없습니다.");
         } else {
-            model.addAttribute("WinStt", winSttInfoDTOS);
+            model.addAttribute("winSttS", winSttInfoDTOS);
         }
 
         return "lotto/checkWinStt";
